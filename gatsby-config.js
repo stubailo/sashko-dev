3308f1eea0fd4e5d6aa1d9b249bd5c3a3f8f5fa2
inhaq <p126013@nu.edu.pk>
2020-04-06 19:31:11 +0500

    add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,13 +69,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Sashko.dev`,
+        short_name: `Sashko`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
