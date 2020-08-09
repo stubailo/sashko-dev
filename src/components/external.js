5d1aac5137da57bf15b7b82fe8f8cf1b40150349
Sashko Stubailo <s.stubailo@gmail.com>
2020-08-08 18:44:35 -0700

    Add graphql mocking external post

--- src/components/external.js
+++ src/components/external.js
@@ -8,5 +8,15 @@ export const externalPost = [
     imgUrl:
       "https://cdn-media-1.freecodecamp.org/images/1*QVTcnfXyMXivNu62IJ7JSg.jpeg",
     external: true
+  },
+  {
+    url:
+      "https://www.freecodecamp.org/news/a-new-approach-to-mocking-graphql-data-1ef49de3d491/",
+    title: "A new approach to mocking GraphQL data",
+    desc: "How we power React component tests and examples at Stripe",
+    date: "2018-12-11",
+    imgUrl:
+      "https://cdn-media-1.freecodecamp.org/images/1*w8fFdZ3gAy7q1x_qOFB-gg.jpeg",
+    external: true
   }
 ];
