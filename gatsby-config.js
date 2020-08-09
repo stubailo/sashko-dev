21d979b1f044fd76d5672fdca36aa4f02dfa8e74
Sashko Stubailo <s.stubailo@gmail.com>
2020-08-08 21:47:25 -0700

    Try adding netlify cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify-cache`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
