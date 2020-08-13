deb5b97526b9d2b6cceb13c2f39be8323bbb6e6d
Sashko Stubailo <s.stubailo@gmail.com>
2020-08-13 07:54:06 -0700

    Add google analytics ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-47580224-5`
       }
     },
     {
