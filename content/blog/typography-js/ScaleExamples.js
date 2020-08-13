6120d1d028627943007dfc6e210cfd657d2b5a6b
Sashko Stubailo <s.stubailo@gmail.com>
2020-08-12 17:57:18 -0700

    Fix tables on mobile

--- content/blog/typography-js/ScaleExamples.js
+++ content/blog/typography-js/ScaleExamples.js
@@ -1,64 +1,75 @@
 import React from "react";
 import { scale } from "../../../src/utils/typography";
-import "./compact-table.css";
+
+function ResponsiveTableContainer({ children }) {
+  return (
+    <div style={{ overflowX: "scroll" }}>
+      <div style={{ minWidth: "800px" }}>{children}</div>
+    </div>
+  );
+}
 
 export function ScaleExamples() {
   return (
-    <table>
-      <tr>
-        <td>Scale</td>
-        <td>Formula</td>
-        <td>fontSize</td>
-        <td>Size in px</td>
-        <td>Example</td>
-      </tr>
-      {[-0.4, -0.2, 0, 0.2, 0.4, 0.6, 1].map(s => (
+    <ResponsiveTableContainer>
+      <table>
         <tr>
-          <td>
-            <code>scale({Math.round(s * 5)} / 5)</code>
-          </td>
-          <td>
-            <code>17 * 2 ^ {s}</code>
-          </td>
-          <td>{scale(s).fontSize}</td>
-          <td>{(parseFloat(scale(s).fontSize, 10) * 17).toFixed(2)}</td>
-          <td style={{ fontSize: scale(s).fontSize }}>Hello, world!</td>
+          <td>Scale</td>
+          <td>Formula</td>
+          <td>fontSize</td>
+          <td>Size in px</td>
+          <td>Example</td>
         </tr>
-      ))}
-    </table>
+        {[-0.4, -0.2, 0, 0.2, 0.4, 0.6, 1].map(s => (
+          <tr>
+            <td>
+              <code>scale({Math.round(s * 5)} / 5)</code>
+            </td>
+            <td>
+              <code>17 * 2 ^ {s}</code>
+            </td>
+            <td>{scale(s).fontSize}</td>
+            <td>{(parseFloat(scale(s).fontSize, 10) * 17).toFixed(2)}</td>
+            <td style={{ fontSize: scale(s).fontSize }}>Hello, world!</td>
+          </tr>
+        ))}
+      </table>
+    </ResponsiveTableContainer>
   );
 }
 
 export function LineHeights() {
   return (
-    <table>
-      <tr>
-        <td>Scale</td>
-        <td>fontSize in px</td>
-        <td>lineHeight in px</td>
-        <td>Example</td>
-      </tr>
-      {[-0.4, 0, 0.2, 0.4, 1, 1.5, 2].map(s => (
+    <ResponsiveTableContainer>
+      <table>
         <tr>
-          <td>
-            <code>scale({s})</code>
-          </td>
-          <td>{(parseFloat(scale(s).fontSize, 10) * 17).toFixed(2)}</td>
-          <td>{(parseFloat(scale(s).lineHeight, 10) * 17).toFixed(2)}</td>
-          <td>
-            <div
-              style={{
-                height: scale(s).lineHeight,
-                lineHeight: scale(s).lineHeight,
-                fontSize: scale(s).fontSize,
-                backgroundColor: "#ffde9c"
-              }}
-            >
-              Hello, world
-            </div>{" "}
-          </td>
+          <td>Scale</td>
+          <td>fontSize in px</td>
+          <td>lineHeight in px</td>
+          <td>Example</td>
         </tr>
-      ))}
-    </table>
+        {[-0.4, 0, 0.2, 0.4, 1, 1.5, 2].map(s => (
+          <tr>
+            <td>
+              <code>scale({s})</code>
+            </td>
+            <td>{(parseFloat(scale(s).fontSize, 10) * 17).toFixed(2)}</td>
+            <td>{(parseFloat(scale(s).lineHeight, 10) * 17).toFixed(2)}</td>
+            <td>
+              <div
+                style={{
+                  height: scale(s).lineHeight,
+                  lineHeight: scale(s).lineHeight,
+                  fontSize: scale(s).fontSize,
+                  backgroundColor: "#ffde9c"
+                }}
+              >
+                Hello, world
+              </div>{" "}
+            </td>
+          </tr>
+        ))}
+      </table>
+    </ResponsiveTableContainer>
   );
 }
